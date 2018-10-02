2b42b34f21253e79ec16685095bd76dc9e323fee
Ryan Blahnik <ryanblahnik@gmail.com>
2018-10-02 18:56:30 -0500

    Add ability to query hosted db with GraphQL

--- server/schema.js
+++ server/schema.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+// const axios = require('axios');
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -7,6 +7,7 @@ const {
   GraphQLList,
   GraphQLNonNull
 } = require('graphql');
+const database = require('../database');
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -33,44 +34,110 @@ const ExpenseType = new GraphQLObjectType({
   })
 });
 
+// stores different queries
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    // initial
     user: {
       type: UserType,
       args: {
       },
       resolve(parentValue, args) {
-        return axios.get(process.env.DB_URI)
-          .then(res => res.data);
+        // return axios.get()
+        //   .then(res => res.data);
       }
     },
     users: {
       type: new GraphQLList(UserType),
       resolve(parentValue, args) {
-        return axios.get(process.env.DB_URI)
-          .then(res => res.data);
+        // return axios.get()
+        //   .then(res => res.data);
       }
     },
+    // in progress - isn't set up to query from args yet
+    // type also refers to returning 1 record and not a list of records
     expense: {
       type: ExpenseType,
       args: {
       },
       resolve(parentValue, args) {
-        return axios.get(process.env.DB_URI)
-          .then(res => res.data);
+        // return axios.get()
+        //   .then(res => res.data);
       }
     },
+    // right now this gets all expenses from the db
+    //   (users aren't connected to expenses yet)
     expenses: {
-      type: new GraphQLList(UserType),
+      type: new GraphQLList(ExpenseType),
       resolve(parentValue, args) {
-        return axios.get(process.env.DB_URI)
-          .then(res => res.data);
+        return database.getExpenses();
       }
     }
   }
 });
 
+// add, edit and delete is all done through mutations
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addCustomer: {
+      type: CustomerType,
+      args: {
+        // NonNull syntax makes a field required
+        name: {type: new GraphQLNonNull(GraphQLString)},
+        email: {type: new GraphQLNonNull(GraphQLString)},
+        age: {type: new GraphQLNonNull(GraphQLInt)}
+      },
+      resolve(parentValue, args) {
+        // // imported db function, or axios request
+        // return axios.post(endpoint, {
+        //   name: args.name,
+        //   email: args.email,
+        //   age: args.age
+        // })
+        // .then(res => res.data);
+      }
+    },
+    deleteCustomer: {
+      type: CustomerType,
+      args: {
+        id
+      },
+      resolve(parentValue, args) {
+        // // imported db function, or axios request
+        // return axios.post(endpoint, {
+        //   name: args.name,
+        //   email: args.email,
+        //   age: args.age
+        // })
+        // .then(res => res.data);
+      }
+    },
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = new GraphQLSchema({
   query: RootQuery
+  mutation
 });
