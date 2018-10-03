ac51133c3b5eb7b9336610085e92ad19c14ad7e6
tae-lim <taelim92@gmail.com>
2018-10-03 11:43:02 -0700

    Comment authentication for functionality

--- server/index.js
+++ server/index.js
@@ -32,27 +32,27 @@ app.use(session({
 
 
 // Setting up passport google oAuth2.0
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clinetSecret: process.env.GOOGLE_CLIENT_SECRET,
-   callbackURL: "https://www.greenwaypay.heroku.com"
-  },
-  function (accessToken, refreshToken, profile, cb) {
-  User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
+// passport.use(new GoogleStrategy({
+//   clientID: process.env.GOOGLE_CLIENT_ID,
+//   clinetSecret: process.env.GOOGLE_CLIENT_SECRET,
+//    callbackURL: "https://www.greenwaypay.heroku.com"
+//   },
+//   function (accessToken, refreshToken, profile, cb) {
+//   User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//       return cb(err, user);
+//     });
+//   }
+// ));
 
-app.get('/auth/google',
-passport.authenticate('google', { scope: ['profile'] }));
+// app.get('/auth/google',
+// passport.authenticate('google', { scope: ['profile'] }));
 
-app.get('/auth/google/callback',
-passport.authenticate('google', { failureRedirect: '/login' }),
-function(req, res) {
-  // Successful authentication, redirect home.
-  res.redirect('/');
-});
+// app.get('/auth/google/callback',
+// passport.authenticate('google', { failureRedirect: '/login' }),
+// function(req, res) {
+//   // Successful authentication, redirect home.
+//   res.redirect('/');
+// });
 
 // app.request('', (request, response) => {
 // };
